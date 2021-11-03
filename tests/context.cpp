603e657e93f2c1ba98151c5028b2a09b2b1b8f1f
Jan Kundrát <jkt@flaska.net>
2021-11-03 20:32:18 +0100

    Initial skeleton for the C++ bindings around libnetconf2
    Change-Id: I823343bec80146c519b29bbb61e6927bea88329c

---
+++ tests/context.cpp
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2021 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Václav Kubernát <kubernat@cesnet.cz>
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+*/
+
+#include <doctest/doctest.h>
+#include <libnetconf2-cpp/Context.hpp>
+#include <memory>
+#include "test_vars.hpp"
+
+TEST_CASE("context")
+{
+    std::optional<libnetconf2::Context> ctx{std::in_place};
+    REQUIRE(ctx);
+}
