195dbdbc3f4111bf3b857ec27125b3db9d48a521
Václav Kubernát <kubernat@cesnet.cz>
2022-01-13 13:58:58 +0100

    Add a test suite
    No we have tests.
    
    Change-Id: Ie7d6dca997b7fcca82daaed9c2f527b99bddeec7

---
+++ tests/mock_server.hpp
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2021 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Václav Kubernát <kubernat@cesnet.cz>
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+*/
+
+#include <boost/process.hpp>
+
+namespace mock_server {
+void skipNetconfChunk(boost::process::ipstream& processOutput);
+void sendRpcReply(int msgId, boost::process::opstream& processInput, std::string data);
+void sendOkReply(int msgId, boost::process::opstream& processInput);
+void handleSessionStart(int& curMsgId, boost::process::opstream& processInput, boost::process::ipstream& processOutput);
+}
