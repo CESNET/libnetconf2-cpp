b1fd592a60077c8f2f52ed77b4fcf309faf2ac07
Jan Kundrát <jan.kundrat@cesnet.cz>
2024-04-10 12:00:54 +0200

    log redirection: support unsetting the handler
    ...and also add some tests that this really works.
    
    Change-Id: I0940f06f996aeb948052f11eb371416de8926608

--- tests/client.cpp
+++ tests/client.cpp
@@ -44,6 +44,9 @@ TEST_CASE("client")
     std::function<std::optional<libyang::DataNode>(std::unique_ptr<libnetconf::client::Session>& session)> testedFunctionality;
     std::string replyData;
     std::string expectedJSON;
+    libnetconf::client::LogCb logCb;
+    std::string logBuf;
+    std::mutex logMtx;
 
     DOCTEST_SUBCASE("get-data")
     {
@@ -88,6 +91,11 @@ TEST_CASE("client")
   }
 }
 )";
+            logCb = [&logBuf, &logMtx](const auto, const auto msg) {
+                std::unique_lock lck{logMtx};
+                logBuf += "\n";
+                logBuf += msg;
+            };
         }
     }
 
@@ -159,6 +167,7 @@ TEST_CASE("client")
     }
 
     libnetconf::client::setLogLevel(libnetconf::LogLevel::Debug);
+    libnetconf::client::setLogCallback(logCb);
     auto x = std::jthread{[&testedFunctionality, &expectedJSON, &processInput, &processOutput] {
         auto ctx = libyang::Context(std::nullopt,
                 libyang::ContextOptions::DisableSearchCwd | libyang::ContextOptions::DisableSearchDirs);
@@ -191,4 +200,11 @@ TEST_CASE("client")
     // For <close-session>
     mock_server::skipNetconfChunk(processOutput, {"<close-session"});
     mock_server::sendRpcReply(curMsgId, processInput, mock_server::OK_REPLY);
+
+    std::unique_lock lck{logMtx};
+    if (logCb) {
+        REQUIRE(logBuf.find("urn:ietf:params:xml:ns:netconf:base:1.0") != std::string::npos);
+    } else {
+        REQUIRE(logBuf.empty());
+    }
 }
