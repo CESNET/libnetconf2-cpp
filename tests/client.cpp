CESNET/libnetconf2-cpp

commit fe77874cba9b046f5be6aca4727a8c32796ff4e0

fix parsing rpc-reply
Upstream commit 01a2614 [1] changed the structure of rpc-error subtree,
some of the nodes stopped being opaque. This broke netconf-cli (see
netconf-cli#25) [2].

it is interesting that some of the nodes created by libnetconf2 are
opaque and some are not, but maintainer confirmed that this is OK.
I decided to alter the pattern for checking if the node is error-message
or error-path by checking for the node's path to avoid
node->isOpaque && node->asOpaque().name().name checks. This way it is
also clear we are looking for rpc-error.

[1] https://github.com/CESNET/libnetconf2/commit/01a26147d4dec94a748cfb5fbb6e9fd4f3da1ff6
[2] https://github.com/CESNET/netconf-cli/issues/25

Bug: https://github.com/CESNET/netconf-cli/issues/25
Change-Id: Icf1510d0c591218873cf579fd1de191da35eefa3

src/netconf-client.cpp
@@ -102,15 +102,16 @@ std::optional<libyang::DataNode> do_rpc(struct nc_session* session, managed_rpc&
             if (!raw_reply) { // <ok> reply, or empty data node, or error
                 std::string msg;
                 for (const auto& child : replyInfo.child()->siblings()) {
-                    if (child.asOpaque().name().name == "rpc-error") {
+                    if (child.path() == "/ietf-netconf:rpc-reply/rpc-error") {
                         for (const auto& error : child.childrenDfs()) {
-                            if (error.asOpaque().name().name == "error-message") {
+                            // Note that error-message and error-path are opaque nodes, other children are not.
+                            if (error.path() == "/ietf-netconf:rpc-reply/rpc-error/error-message") {
                                 msg += "Error: ";
                                 msg += error.asOpaque().value();
                                 msg += "\n";
                             }
 
-                            if (error.asOpaque().name().name == "error-path") {
+                            if (error.path() == "/ietf-netconf:rpc-reply/rpc-error/error-path") {
                                 msg += "Path: ";
                                 msg += error.asOpaque().value();
                                 msg += "\n";

tests/client.cpp
@@ -158,6 +158,27 @@ TEST_CASE("client")
         replyData = mock_server::OK_REPLY;
     }
 
+    DOCTEST_SUBCASE("NETCONF rpc replies with rpc-error")
+    {
+        testedFunctionality = [](std::unique_ptr<libnetconf::client::Session>& session) {
+            REQUIRE_THROWS_WITH_AS(
+                session->editData(libnetconf::NmdaDatastore::Running, R"(<myLeaf xmlns="http://example.com">AHOJ</myLeaf>)"),
+                "Path: /cla-roadm:media-channels[cla-roadm:channel='16 (GHz)']\n"
+                "Error: Access to the data model \"czechlight-roadm-device\" is denied because \"tomas\" NACM authorization failed.\n",
+                libnetconf::client::ReportedError);
+            return std::nullopt;
+        };
+
+        replyData = R"(<rpc-error>
+  <error-type>protocol</error-type>
+  <error-tag>access-denied</error-tag>
+  <error-severity>error</error-severity>
+  <error-path xmlns:cla-roadm="http://czechlight.cesnet.cz/yang/czechlight-roadm-device">/cla-roadm:media-channels[cla-roadm:channel='16 (GHz)']</error-path>
+  <error-message xml:lang="en">Access to the data model "czechlight-roadm-device" is denied because "tomas" NACM authorization failed.</error-message>
+</rpc-error>
+)";
+    }
+
     DOCTEST_SUBCASE("editConfig")
     {
         testedFunctionality = [] (std::unique_ptr<libnetconf::client::Session>& session) {

commit 5f22d3fb2e184ab3c2d9334af26dbb6da560e169

another fix for parsing error-path node
The previous commit fe77874cb ("fix parsing rpc-reply") was wrong.
error-path node is *not* opaque as was stated by the commit's message
and it can be seen in the linked upstream commit [1].

The tests worked, because the error-path content was set to a path in an
unimplemented YANG model. The node was then parsed as opaque.

This patch implements checking if error-path is opaque or not and parses
it accordingly.

[1] https://github.com/CESNET/libnetconf2/blob/01a26147d4dec94a748cfb5fbb6e9fd4f3da1ff6/src/messages_server.c#L587

Fixes: fe77874cba9b046f5be6aca4727a8c32796ff4e0 ("fix parsing rpc-reply")
Change-Id: I80dbe4b9f544022ae108060e2f5d9fd06edef3f6

src/netconf-client.cpp
@@ -104,7 +104,7 @@ std::optional<libyang::DataNode> do_rpc(struct nc_session* session, managed_rpc&
                 for (const auto& child : replyInfo.child()->siblings()) {
                     if (child.path() == "/ietf-netconf:rpc-reply/rpc-error") {
                         for (const auto& error : child.childrenDfs()) {
-                            // Note that error-message and error-path are opaque nodes, other children are not.
+                            // Note that error-message is opaque node, rpc-path is opaque node when server sends a response with path from unimplemented models
                             if (error.path() == "/ietf-netconf:rpc-reply/rpc-error/error-message") {
                                 msg += "Error: ";
                                 msg += error.asOpaque().value();
@@ -113,7 +113,7 @@ std::optional<libyang::DataNode> do_rpc(struct nc_session* session, managed_rpc&
 
                             if (error.path() == "/ietf-netconf:rpc-reply/rpc-error/error-path") {
                                 msg += "Path: ";
-                                msg += error.asOpaque().value();
+                                msg += error.isOpaque() ? error.asOpaque().value() : error.asTerm().valueStr();
                                 msg += "\n";
                             }
                         }

tests/client.cpp
@@ -160,16 +160,39 @@ TEST_CASE("client")
 
     DOCTEST_SUBCASE("NETCONF rpc replies with rpc-error")
     {
-        testedFunctionality = [](std::unique_ptr<libnetconf::client::Session>& session) {
-            REQUIRE_THROWS_WITH_AS(
-                session->editData(libnetconf::NmdaDatastore::Running, R"(<myLeaf xmlns="http://example.com">AHOJ</myLeaf>)"),
-                "Path: /cla-roadm:media-channels[cla-roadm:channel='16 (GHz)']\n"
-                "Error: Access to the data model \"czechlight-roadm-device\" is denied because \"tomas\" NACM authorization failed.\n",
-                libnetconf::client::ReportedError);
-            return std::nullopt;
-        };
+        DOCTEST_SUBCASE("rpc-path")
+        {
+            testedFunctionality = [](std::unique_ptr<libnetconf::client::Session>& session) {
+                REQUIRE_THROWS_WITH_AS(
+                    session->editData(libnetconf::NmdaDatastore::Running, R"(<myLeaf xmlns="http://example.com">AHOJ</myLeaf>)"),
+                    "Path: /example-schema:leafInt8\n"
+                    "Error: Access to the data model \"example-schema\" is denied because \"tomas\" NACM authorization failed.\n",
+                    libnetconf::client::ReportedError);
+                return std::nullopt;
+            };
 
-        replyData = R"(<rpc-error>
+            replyData = R"(<rpc-error>
+  <error-type>protocol</error-type>
+  <error-tag>access-denied</error-tag>
+  <error-severity>error</error-severity>
+  <error-path xmlns:aha="http://example.com">/aha:leafInt8</error-path>
+  <error-message xml:lang="en">Access to the data model "example-schema" is denied because "tomas" NACM authorization failed.</error-message>
+</rpc-error>
+)";
+        }
+
+        DOCTEST_SUBCASE("rpc-path contains invalid path")
+        {
+            testedFunctionality = [](std::unique_ptr<libnetconf::client::Session>& session) {
+                REQUIRE_THROWS_WITH_AS(
+                    session->editData(libnetconf::NmdaDatastore::Running, R"(<myLeaf xmlns="http://example.com">AHOJ</myLeaf>)"),
+                    "Path: /cla-roadm:media-channels[cla-roadm:channel='16 (GHz)']\n"
+                    "Error: Access to the data model \"czechlight-roadm-device\" is denied because \"tomas\" NACM authorization failed.\n",
+                    libnetconf::client::ReportedError);
+                return std::nullopt;
+            };
+
+            replyData = R"(<rpc-error>
   <error-type>protocol</error-type>
   <error-tag>access-denied</error-tag>
   <error-severity>error</error-severity>
@@ -177,6 +200,7 @@ TEST_CASE("client")
   <error-message xml:lang="en">Access to the data model "czechlight-roadm-device" is denied because "tomas" NACM authorization failed.</error-message>
 </rpc-error>
 )";
+        }
     }
 
     DOCTEST_SUBCASE("editConfig")